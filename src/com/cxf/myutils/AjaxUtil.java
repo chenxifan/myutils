80b9fcd128c91ea12691d6a61915d37543360b55
chenxifan <chenxifan2010@163.com>
2014-04-29 14:31:32 +0800

    servlet 2.5

--- src/com/cxf/myutils/AjaxUtil.java
+++ src/com/cxf/myutils/AjaxUtil.java
@@ -1,10 +1,7 @@
 /*
  * 创建日期 2012-7-31
- *
- * TODO 要更改此生成的文件的模板，请转至
- * 窗口 － 首选项 － Java － 代码样式 － 代码模板
  */
-package com.excellence.expps.util;
+package com.cxf.myutils;
 
 import java.io.IOException;
 import java.io.PrintWriter;
